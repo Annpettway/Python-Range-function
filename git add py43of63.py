7d3c63d13289c30c7541b73387fe376d55267eec
Annpettway <Developernajem@yahoo.com>
2016-10-14 00:31:06 -0500

    Update git add py43of63.py

--- git add py43of63.py
+++ git add py43of63.py
@@ -1,3 +1,8 @@
 # Python-Range-function
 Creates a list of index numbers that will be used in a for loop
 (use "git add py43of63.py to include in what will be committed)
+
+ 
+ 
+ git remote add origin https://github.com/Annpettway/Python-Range-function.git
+git push -u origin master
